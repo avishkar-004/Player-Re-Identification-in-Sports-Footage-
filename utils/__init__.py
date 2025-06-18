32a95dfb83a3b08f6c9e2e8a4598908526bb6df9
Avishkar Pawar <avishkarpawar004@gmail.com>
2025-06-18 09:15:38 +0530

    chore: add version string to utils package

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,3 +2,5 @@
 from .detection import Detector
 from .tracking import EnhancedTracker
 from .visualization import EnhancedVisualizer
+# Utils package for player re-identification pipeline
+__version__ = '1.0.0'
